ea3a06325360d748d9a9655468458ca8cf1d657e
raghurazy <135259028+raghurazy@users.noreply.github.com>
2023-06-04 10:18:00 +0530

    Add files via upload
    Get element by class name.

--- dom.js
+++ dom.js
@@ -1,3 +1,16 @@
-var headerTitle = document.getElementById('main-header');
+// var headerTitle = document.getElementById('main-header');
 
-headerTitle.style.borderBottom = 'solid 3px #000'
+// headerTitle.style.borderBottom = 'solid 3px #000'
+
+// console.log(document.all);
+
+let items = document.getElementsByClassName('list-group-item')
+console.log(items);
+items[1].textContent = 'hello 2';
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = '#f4f4f4';
+
+for (let i = 0 ; i<items.length ; i++){
+    items[2].style.backgroundColor = 'green';
+    items[i].style.fontWeight = 'bold'
+}
