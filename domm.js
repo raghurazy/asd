f233481b563558aea5a894ea2c6920483caef939
raghurazy <135259028+raghurazy@users.noreply.github.com>
2023-06-05 07:01:30 +0530

    Add files via upload

--- domm.js
+++ domm.js
@@ -34,7 +34,7 @@
 
 // // Traversing the node
 
-var ItemList = document.querySelector('#items');
+// var ItemList = document.querySelector('#items');
 // // console.log(ItemList.parentNode);
 // // ItemList.parentNode.style.backgroundColor = '#f4f4f4';
 // // console.log(ItemList.parentNode.parentNode.parentNode);
@@ -47,15 +47,84 @@ var ItemList = document.querySelector('#items');
 // ItemList.children[1].style.backgroundColor = 'yellow';
 // ItemList.firstElementChild.textContent = 'Hello 1';
 
-// ItemList.lastElementChild.textContent = 'Hello 4'
-console.log(ItemList.nextElementSibling);
-console.log(ItemList.previousElementSibling)
-console.log(ItemList.previousSibling)
+// // ItemList.lastElementChild.textContent = 'Hello 4'
+// console.log(ItemList.nextElementSibling);
+// console.log(ItemList.previousElementSibling)
+// console.log(ItemList.previousSibling)
 
-// create element
-var newdiv = document.createElement('div')
+// // create element
+// var newdiv = document.createElement('div')
 
-newdiv.className = 'hello'
+// newdiv.className = 'hello'
 
-newdiv.id = 'hello 1'
-console.log(newdiv);
+// newdiv.id = 'hello 1'
+// console.log(newdiv);
+
+
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
+
+// Add item
+function addItem(e){
+  e.preventDefault();
+
+  // Get input value
+  var newItem = document.getElementById('item').value;
+
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
+
+  // Create del button element
+  var deleteBtn = document.createElement('button');
+
+  // Add classes to del button
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  // Append button to li
+  li.appendChild(deleteBtn);
+
+  // Append li to list
+  itemList.appendChild(li);
+}
+
+// Remove item
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+
+// Filter Items
+function filterItems(e){
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
